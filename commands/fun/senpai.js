635b953eb048f50fcfe12a5cce503e2cee21d70a
Tarren <26067973+EpokTarren@users.noreply.github.com>
2020-02-09 23:01:11 +0100

    Added senpai command + sqlite3
    Added senpai command and sqlite3 as a dependency, added .db files to .gitnore so that they do not end up on github

---
+++ commands/fun/senpai.js
@@ -0,0 +1,31 @@
+let sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./commands/fun/senpai.db', (err) => {
+	if (err) {
+		return console.error(err.message);
+	}
+	console.log('Connected to senpai SQlite database.');
+});
+db.run(`CREATE TABLE IF NOT EXISTS senpais(kouhai TEXT UNIQUE NOT NULL, senpai TEXT NOT NULL);`);
+module.exports = {
+	run: function (message, args) {
+		if(args[1]&&args[1].startsWith('del'))
+			return db.run(`DELETE FROM senpais WHERE kouhai = ${message.author.id};`), message.channel.send('', {embed: {color: 0xff0000, title: 'Senpai cleared'}});
+		if(message.mentions.users.first())
+			db.run(`INSERT OR REPLACE INTO senpais(kouhai, senpai) VALUES (${message.author.id}, ${message.mentions.users.first().id})`,), message.channel.send('Senpai set');
+		else
+			db.all(`SELECT senpai FROM senpais WHERE kouhai = ${message.author.id}`, (err, rows)=>{
+				if(err)
+					console.log(err);
+				else
+					if(rows[0])
+						message.channel.send('',{embed: {color: 0xff80cc, title: `Your senpai is ${message.guild.members.get(rows[0].senpai).displayName||rows[0].senpai}`}});
+					else
+						message.channel.send('',{embed: {color: 0xff0000, title: 'You have no senpai!'}});
+			});
+	},
+	description: 'Sets, clears, and displays your senpai',
+	detailed: 'Lets you set, display, modify, and clear your senpai to your hearts content',
+	examples: prefix => `${prefix}senpai @someone, ${prefix}senpai del, ${prefix}senpai`,
+	name: 'senpai',
+	perms: null,
+};
