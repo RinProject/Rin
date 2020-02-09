28eddd3bbcb3168f178b7569474335dd3d496937
vAnomali <54195092+vAnomali@users.noreply.github.com>
2020-02-09 17:31:21 -0500

    Update ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,32 +1,50 @@
 module.exports = {
 	async run(message) {
-		if (message.mentions.members.size()){
+		if (message.mentions.members.first()){
 			message.mentions.members.tap(member => {
-				let membersRole = message.guild.roles.find('name', 'test');//Need to replace test with an actual role that should have immunity(mods/admins)
+				let membersRole = message.guild.roles.find('name', 'Cutie');//Need to replace test with an actual role that should have immunity(mods/admins)
 				if(!message.mentions.roles.has(membersRole.id)) {
 					member.ban(0).then(() => {
-						message.channel.send(newRichEmbed()
+						/*message.channel.send(new RichEmbed()
 							.setColor('GREEN')
 							.setTitle(`${member.user.tag} has been banned by ${message.author.tag}.`)
                             .setImage(message.mentions.avatarURL)
-						);
+						);*/
+								message.channel.send('', {embed: {
+										title:`${member.user.tag} has been banned by ${message.author.tag}.`,
+										color: 0x00FF00
+							  }});
+						}
 					}).catch(() => {
-						message.channel.send(new RichEmbed()
+						/*message.channel.send(new RichEmbed()
 							.setColor('RED')
 							.setTitle(`${member.user.tag} was not banned. Reason: unknown.`)
-					)});
+					)});*/
+								message.channel.send('', {embed: {
+										title:`${member.user.tag} was not banned. Reason: unknown.`,
+										color: 0xFF0000
+								}});
 				} else {
-					message.channel.send(new RichEmbed()
+					/*message.channel.send(new RichEmbed()
                         .setColor('ORANGE')
                         .setTitle(`${member.user.tag} was not banned. Reason: Member cannot be banned by bot.`)
-                    )
+                    )*/
+										message.channel.send('', {embed: {
+												title:`${member.user.tag} was not banned. Reason: Member cannot be banned by bot.`,
+												color: 0xFF0000
+										}});
 				}
 			});
 		} else {
+			/*
 			return message.channel.send(new RichEmbed()
                 .setColor('RED')
                 .setTitle(`Please mention users to ban.`)
-            );
+            );*/
+						message.channel.send('', {embed: {
+								title:`Please mention users to ban.`,
+								color: 0xFF0000
+						}});
 		}
 	},
   description: 'Bans a user',
