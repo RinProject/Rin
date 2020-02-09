6661352b661d9e5c036e4b537f44c842d05538b1
vAnomali <54195092+vAnomali@users.noreply.github.com>
2020-02-09 16:16:13 -0500

    Create ban.js

---
+++ moderation/ban.js
@@ -0,0 +1,32 @@
+module.exports = {
+	async run(message) {
+		if message.mentions.members.size(){
+			message.mentions.members.tap(member => {
+				let membersRole = message.guild.roles.find('name', 'test');//Need to replace test with an actual role that should have immunity(mods/admins)
+				if(!message.mentions.roles.has(membersRole.id) {
+					member.ban(0).then(() ==> {
+						message.channel.send(newRichEmbed()
+							.setColor('GREEN')
+							.setTitle(`${member.user.tag} has been banned by ${message.author.tag}.`)
+                            .setImage(message.mentions.avatarURL)
+						);
+					}).catch(() => {
+						message.channel.send(new RichEmbed()
+							.setColor('RED')
+							.setTitle(`${member.user.tag} was not banned. Reason: unknown.`)
+					)});
+				} else {
+					message.channel.send(new RichEmbed()
+                        .setColor('ORANGE')
+                        .setTitle(`${member.user.tag} was not banned. Reason: Member cannot be banned by bot.`)
+                    )
+				}
+			});
+		} else {
+			return message.channel.send(new RichEmbed()
+                .setColor('RED')
+                .setTitle(`Please mention users to ban.`)
+            );
+		}
+	}
+}
