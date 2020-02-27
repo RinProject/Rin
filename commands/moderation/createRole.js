df2bb82221f47d01201d15c82b0577562dd01eb5
vAnomali <54195092+vAnomali@users.noreply.github.com>
2020-02-26 19:04:34 -0500

    Update createRole.js

--- commands/moderation/createRole.js
+++ commands/moderation/createRole.js
@@ -1,15 +1,14 @@
 module.exports = {
-    async run (message, args) {
-      if (!args[1]) return;
-
-      mesage.member.guild.createRole({
-        name: args[1],
-        color: parseInt(args[2].replace('#',''),16)
-      });
-    },
-       description: 'Creates a role',
-       detailed: 'Creates a role: Takes in name and color',
-       examples: prefix => ${prefix}createrole @rolename #hexcolor,
-       name: 'createrole',
-       perms: ['MANAGE_ROLES']
+   async run (message, args) {
+     if(!args[1]) return;
+     message.member.guild.createRole({
+       name: args[1],
+       color: parseInt(args[2].replace('#',''), 16)||0x0000ff
+     });
+   },
+   description: 'Creates a role',
+   detailed: 'Creates a role: Takes in name and color',
+   examples: prefix => `${prefix}createrole @rolename #hexcolor`,
+   name: 'createrole',
+   perms: ['MANAGE_ROLES']
 }
