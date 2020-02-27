4cd73d84103dacbaa7c4898c9e7c277152b43094
vAnomali <54195092+vAnomali@users.noreply.github.com>
2020-02-26 19:30:41 -0500

    Create lock.js

---
+++ commands/moderation/lock.js
@@ -0,0 +1,35 @@
+module.exports = {
+    async run (message) {
+      const everyone = message.guild.defaultRole.id;
+      if (message.content.split(' ')[1] != 'off') {
+        message.channel.overwritePermissions(everyone, {
+          'SEND_MESSAGES': false
+        }).then(channel => {
+          message.channel.send('', {
+            embed: {
+              title: 'Channel Locked',
+              color: 0xFF0000
+            }
+          });
+        });
+      } else {
+        if (message.content.split(' ')[1] == 'off') {
+          message.channel.overwritePermissions(everyone, {
+            'SEND_MESSAGES': true
+          }).then(channel => {
+            message.channel.send('', {
+              embed: {
+                title: 'Channel Unlocked',
+                color: 0x00FF00
+              }
+            });
+          });
+        }
+      }
+    },
+    description: 'Locks a channel',
+    detailed: 'Overwrites permissions to speak in channel it is called in',
+    examples: prefix => `${prefix} lock, prefix => $prefix} lock off`,
+    name: `lock`,
+    perms: ['MANAGE_ROLES']
+}
