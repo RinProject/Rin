7a8736d0b2326c299aebc6a72c218899b941127c
ItsSoze <61681911+ItsSoze@users.noreply.github.com>
2020-03-03 01:16:00 -0500

    Added error embed if lacking perms
    The bot will now output an error message if it lacks permissions, rather than throwing an unhandled promise rejection.

--- commands/moderation/createRole.js
+++ commands/moderation/createRole.js
@@ -1,5 +1,17 @@
 module.exports = {
    async run (message, args) {
+
+     // Check for bot permissions
+     if (!message.guild.me.hasPermission('MANAGE_ROLES')) {
+      return message.channel.send('', {
+        embed: {
+          title: `An error occured.`,
+          description: "I need the ``manage roles`` permission to create roles.",
+          color: 0xFF0000
+        }
+      });
+    }
+
      if(!args[1]) return;
      message.member.guild.createRole({
        name: args[1],
