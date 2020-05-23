0b0cd830dd727dc89fdb800aa90da33df764890c
vAnomali <54195092+vAnomali@users.noreply.github.com>
2020-05-23 14:24:46 -0400

    Fixed Ban and Kick, Wrote Unban

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,45 +1,28 @@
 module.exports = {
-	async run(message) {
-		if (message.mentions.members.first()) {
-			message.mentions.members.tap(member => {
-				if (message.guild.me.hasPermission('BAN_MEMBERS')) {
+	async run(message, args) {
+			let member = message.mentions.members.first() || await message.guild.members.cache.get(args[1]);
+			if (member == undefined) {
+				return message.channel.send('', {
+					embed: {
+							"title" : "Mention a user to ban",
+							"color" : 0xFF0000
+					}
+				});
+			}
+			else {
 					member.ban(0).then(() => {
 						message.channel.send('', {
 							embed: {
-								title: `${member.user.tag} has been banned by ${message.author.tag}.`,
-								color: 0x00FF00
+									"title" : `${member.user.tag} has been banned by ${message.author.tag}.`,
+									"color" : 0x00FF00
 							}
 						});
-					}
-					).catch(() => {
-						message.channel.send('', {
-							embed: {
-								title: `${member.user.tag} was not banned. Reason: unknown.`,
-								color: 0xFF0000
-							}
-						})
 					})
-				} else {
-					message.channel.send('', {
-						embed: {
-							title: `${member.user.tag} was not banned. Reason: Member cannot be banned by bot. Check my status in the servers role hierarchy.`,
-							color: 0xFF0000
-						}
-					});
-				}
-			});
-		} else {
-			message.channel.send('', {
-				embed: {
-					title: `Please mention users to ban.`,
-					color: 0xFF0000
-				}
-			});
-		}
+			}
 	},
 	description: 'Bans a user',
 	detailed: 'Bans all users mentioned',
-	examples: prefix => `${prefix}ban @someone1, @someone2, @someone3`,
+	examples: prefix => `${prefix}ban @someone, ${prefix}ban <id>`,
 	name: 'ban',
 	perms: [`BAN_MEMBERS`]
 }
--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -1,45 +1,28 @@
 module.exports = {
-	async run(message) {
-		if (message.mentions.members.first()) {
-			message.mentions.members.tap(member => {
-				if (message.guild.me.hasPermission('KICK_MEMBERS')) {
+	async run(message, args) {
+			let member = message.mentions.members.first() || await message.guild.members.cache.get(args[1]);
+			if (member == undefined) {
+				return message.channel.send('', {
+					embed: {
+						"title" : "Mention a user to kick",
+						"color" : 0xFF0000
+					}
+				});
+			}
+			else {
 					member.kick().then(() => {
 						message.channel.send('', {
 							embed: {
-								title: `${member.user.tag} has been kicked by ${message.author.tag}.`,
-								color: 0x00FF00
+								"title" : `${member.user.tag} has been kicked by ${message.author.tag}.`,
+								"color" : 0x00FF00
 							}
 						});
-					}
-					).catch(() => {
-						message.channel.send('', {
-							embed: {
-								title: `${member.user.tag} was not kicked. Reason: unknown.`,
-								color: 0xFF0000
-							}
-						})
 					})
-				} else {
-					message.channel.send('', {
-						embed: {
-							title: `${member.user.tag} was not kicked. Reason: Member cannot be kicked by bot. Check my status in the servers role hierarchy.`,
-							color: 0xFF0000
-						}
-					});
-				}
-			});
-		} else {
-			message.channel.send('', {
-				embed: {
-					title: `Please mention users to kick.`,
-					color: 0xFF0000
-				}
-			});
-		}
+			}
 	},
 	description: 'Kicks a user',
-	detailed: 'Kicks all users mentioned',
-	examples: prefix => `${prefix}kick @someone1, @someone2, @someone3`,
+	detailed: 'Kicks mentioned user',
+	examples: prefix => `${prefix}kick @someone, ${prefix}kick <id>`,
 	name: 'kick',
 	perms: [`KICK_MEMBERS`]
 }
