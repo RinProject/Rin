831cea1ee1afb5bcc521c4f16aa0deb02dcdb9ec
vAnomali <54195092+vAnomali@users.noreply.github.com>
2020-05-24 14:14:56 -0400

    Create avatar.js

---
+++ commands/utility/avatar.js
@@ -0,0 +1,35 @@
+module.exports = {
+	async run(message, args) {
+		let member = message.mentions.members.first() || await message.guild.members.cache.get(args[1]);
+		if (member == undefined) {
+			return message.channel.send('', {
+				embed: {
+					title: `${message.author.username}'s Avatar`,
+					color: 0xFF80CC,
+					image: {
+						url: message.author.avatarURL(),
+						height: 512, 
+						width: 512
+					}
+				}
+			});
+		} else {
+			return message.channel.send('', {
+				embed: {
+					title: `${message.mentions.users.first().username}'s Avatar`,
+					color: 0xFF80CC,
+					image: {
+						url: message.mentions.users.first().avatarURL(),
+						height: 512, 
+						width: 512
+					}
+				}
+			})
+		}
+	},
+	description: 'Provides a users avatar',
+	detailed: 'Provides a users avatar',
+	examples: prefix => `${prefix}avatar @user, ${prefix}avatar <user id>`,
+	name: 'avatar',
+	perms: [null]
+}
