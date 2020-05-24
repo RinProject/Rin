e4e174682fab20aa262b94a09900d1e335d4aa1a
ItsSoze <61681911+ItsSoze@users.noreply.github.com>
2020-05-24 18:53:13 -0400

    Created ping command (only for msg latency)
    Created ping command that gets message latency (unable to do API latency, will bother Tarren later about it)

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,15 +1,24 @@
 module.exports = {
-    async run(message) {
-        let ping = message.createdTimestamp - Date.now();
-        return message.channel.send('', {
-            embed: {
-                title: `${ping}ms`,
-                color: 0xFF80CC
-            }
-        });
-    },
-    description: 'Provides the bots ping',
-	detailed: 'Provides the bots ping',
+	async run(message, args) {
+    message.channel.send('', {
+		embed: {
+                title: `Calculating ping..`,
+		color: 0xFF80CC
+		}
+		}
+		).then(
+            msg => {
+                msg.edit('', {
+                embed: {
+                    title: `Ping`,
+                    description: `The bot latency is ${(msg.createdTimestamp - message.createdTimestamp)}ms`,
+                    color: 0xFF80CC
+                }})
+                }
+        )
+	},
+	description: 'Provides ping',
+	detailed: 'Provides latency for the bot',
 	examples: prefix => `${prefix}ping`,
 	name: 'ping',
 	perms: [null]
