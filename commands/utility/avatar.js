74e2975df22bdba4bee582e47bbbe9202050aee2
Tarren <26067973+EpokTarren@users.noreply.github.com>
2020-05-24 22:42:09 +0200

    Remove pointless code

--- commands/utility/avatar.js
+++ commands/utility/avatar.js
@@ -1,31 +1,17 @@
 module.exports = {
 	async run(message, args) {
-		let member = message.mentions.members.first() || await message.guild.members.cache.get(args[1]);
-		if (member == undefined) {
-			return message.channel.send('', {
-				embed: {
-					title: `${message.author.username}'s Avatar`,
-					color: 0xFF80CC,
-					image: {
-						url: message.author.displayAvatarURL(),
-						height: 512, 
-						width: 512
-					}
+		let member = message.mentions.members.first() || await message.guild.members.cache.get(args[1]) || message.member;
+		message.channel.send('', {
+			embed: {
+				title: `${member.user.username}'s Avatar`,
+				color: 0xFF80CC,
+				image: {
+					url: member.user.avatarURL(),
+					height: 512, 
+					width: 512
 				}
-			});
-		} else {
-			return message.channel.send('', {
-				embed: {
-					title: `${member.user.username}'s Avatar`,
-					color: 0xFF80CC,
-					image: {
-						url: member.user.avatarURL(),
-						height: 512, 
-						width: 512
-					}
-				}
-			})
-		}
+			}
+		})
 	},
 	description: 'Provides a users avatar',
 	detailed: 'Provides a users avatar',
