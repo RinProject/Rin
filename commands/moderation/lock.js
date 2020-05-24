50dbbdd19a41de8447c0934ce31b3ef76eefa9f1
Tarren <26067973+EpokTarren@users.noreply.github.com>
2020-05-24 20:12:58 +0200

    Fixed commands that weren't working because of v12

--- commands/moderation/lock.js
+++ commands/moderation/lock.js
@@ -1,35 +1,34 @@
 module.exports = {
-    async run (message) {
-      const everyone = message.guild.defaultRole.id;0
-      if (message.content.split(' ')[1] != 'off') {
-        message.channel.overwritePermissions(everyone, {
-          'SEND_MESSAGES': false
-        }).then(channel => {
-          message.channel.send('', {
-            embed: {
-              title: 'Channel Locked',
-              color: 0xFF0000
-            }
-          });
-        });
-      } else {
-        if (message.content.split(' ')[1] == 'off') {
-          message.channel.overwritePermissions(everyone, {
-            'SEND_MESSAGES': true
-          }).then(channel => {
-            message.channel.send('', {
-              embed: {
-                title: 'Channel Unlocked',
-                color: 0x00FF00
-              }
-            });
-          });
-        }
-      }
-    },
-    description: 'Locks a channel',
-    detailed: 'Overwrites permissions to speak in channel it is called in',
-    examples: prefix => `${prefix} lock, prefix => $prefix} lock off`,
-    name: `lock`,
-    perms: ['MANAGE_ROLES']
+	async run(message, args) {
+		if (args[1] != 'off') {
+			message.channel.overwritePermissions([{
+					id: message.guild.id,
+					deny: ['SEND_MESSAGES']
+			}]).then(channel => {
+				message.channel.send('', {
+					embed: {
+						title: 'Channel Locked',
+						color: 0xFF0000
+					}
+				});
+			});
+		} else {
+			message.channel.overwritePermissions([{
+				id: message.guild.id,
+				allow: ['SEND_MESSAGES']
+			}]).then(channel => {
+				message.channel.send('', {
+					embed: {
+						title: 'Channel Unlocked',
+						color: 0x00FF00
+					}
+				});
+			});
+		}
+	},
+	description: 'Locks a channel',
+	detailed: 'Overwrites permissions to speak in channel it is called in',
+	examples: prefix => `${prefix} lock, prefix => $prefix} lock off`,
+	name: `lock`,
+	perms: ['MANAGE_ROLES']
 }
--- commands/moderation/roleColor.js
+++ commands/moderation/roleColor.js
@@ -1,35 +1,48 @@
 module.exports = {
-  async run (message, args) {
-    if (args[1] == undefined){
-        return message.channel.send('', {
-          embed: {
-            "title":"Please enter a role, followed by a color.",
-            "color":0xFF0000
-          }
-        });
-    } else if (args[2] == undefined){
-      return message.channel.send('', {
-        embed: {
-          "title":"Please enter a role, followed by a color.",
-          "color":0xFF0000
-        }
-      });
-    } else {
-    let role = message.guild.roles.find('name', args[1]);
-      role.edit({
-              color: args[2]
-      });
-      return message.channel.send('', {
-        embed: {
-            "title": `Role ${args[1]} color changed to ${args[2]}`,
-            "color": args[2]
-        }
-      });
-    }
-  },
-  description: 'Changes a Roles Color',
-  detailed: 'Changes the color of a preexisting role',
-  examples: prefix => `${prefix}rolecolor @rolename #hexcolor`,
-  name: 'rolecolor',
-  perms: ['MANAGE_ROLES']
+	async run(message, args) {
+		if (args[1] == undefined || args[2] == undefined) {
+			return message.channel.send('', {
+				embed: {
+					title: 'Invalid number of inputs',
+					description: 'Please enter a role, followed by a colour.\nExamples: `members #FF8000`, `@coolKids #FF80CC`',
+					color: 0xFF0000
+				}
+			});
+		} else if(!args[2].match(/#?[0-9a-f]{6}/gi))
+			return message.channel.send('', {
+				embed: {
+					title: 'Invalid colour',
+					description: 'Please enter a colour in hex format.\nExample: #FF8000.',
+					color: 0xFF0000
+				}
+			});
+		else {
+			let role = message.mentions.roles.first() || message.guild.roles.cache.find(role => role.name === args[1]);
+			if(role == undefined)
+				return message.channel.send('', {
+					embed: {
+						title: "Could not find role.",
+						description: 'Please provide a valid role, either by @mention or by name.\nNote: names may not contain whitespace, in such cases us @mention',
+						color: 0xFF0000
+					}
+				});
+			let color = args[2].startsWith('#') ? args[2] : '#' + args[2];
+			let oldColor = role.hexColor;
+			role.edit({
+				color: color
+			});
+			return message.channel.send('', {
+				embed: {
+					title: `Role ${args[1]} colour updated`,
+					description: `Colour changed from ${oldColor} to ${color}`,
+					color: color
+				}
+			});
+		}
+	},
+	description: 'Changes a Roles Colour',
+	detailed: 'Changes the colour of a preexisting role',
+	examples: prefix => `${prefix}rolecolor @rolename #hexcolor`,
+	name: 'rolecolor',
+	perms: ['MANAGE_ROLES']
 }
