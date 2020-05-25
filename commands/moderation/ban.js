156f63db10b34e9260d9fae87708c2f8d394f57c
Tarren <26067973+EpokTarren@users.noreply.github.com>
2020-05-25 20:09:28 +0200

    Made ban and unban work and look nice

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,24 +1,49 @@
 module.exports = {
 	async run(message, args) {
-			let member = message.mentions.members.first() || await message.guild.members.cache.get(args[1]);
-			if (member == undefined) {
-				return message.channel.send('', {
+		if (!message.guild.me.hasPermission('BAN_MEMBERS')) {
+			return message.channel.send('', {
+				embed: {
+					title: 'An error occurred.',
+					description: 'I require the **ban members** permission to ban users.',
+					color: 0xCC1020
+				}
+			})
+		}
+		let user = message.mentions.users.first() || await message.client.users.fetch(args[1])
+		.catch(e => {
+			if (e) {
+				message.channel.send('', {
 					embed: {
-							"title" : "Mention a user to ban",
-							"color" : 0xFF0000
+						title: 'Please provide a user to ban.',
+						color: 0xCC1020
 					}
-				});
+				})
 			}
-			else {
-					member.ban(0).then(() => {
-						message.channel.send('', {
-							embed: {
-									"title" : `${member.user.tag} has been banned by ${message.author.tag}.`,
-									"color" : 0x00FF00
-							}
-						});
-					})
-			}
+		});
+
+		if (user == undefined) {
+			return;
+		}
+
+		message.guild.members.ban(user, 0).then(()=>{
+			message.channel.send('', {
+				embed: {
+					title: 'User successfully banned',
+					description: `${user.tag} has been banned by ${message.author.tag}.`,
+					color: 0x10CC10,
+					footer: {
+						text: `id: ${user.id}`
+					}
+				}
+			});
+		}).catch(e => {
+			message.channel.send('', {
+				embed: {
+					title: 'Failed to ban user.',
+					color: 0xCC1020
+				}
+			});
+		});
 	},
 	description: 'Bans a user',
 	detailed: 'Bans all users mentioned',
--- commands/moderation/unban.js
+++ commands/moderation/unban.js
@@ -1,36 +1,40 @@
 module.exports = {
-    async run (message, args) {
-        let bans = await message.guild.fetchBans();
-        let bannedPerson = bans.get(args[1]);
+	async run(message, args) {
+		let bans = await message.guild.fetchBans();
+		let bannedPerson = bans.get(args[1]);
 
-        console.log(bannedPerson)
-        if (bannedPerson == undefined) {
-          return message.channel.send('', {
-            embed: {
-                "title" : "Please provide a user ID to unban",
-                "color" : 0xFF0000
-            }
-          });
-        }
-        let reason = args.slice(1).join(" ");
-        if(!reason) {
-          reason = "No reason provided";
-        }
-        try {
-          message.guild.members.unban(bannedPerson.user.id, {Reason: reason})
-          message.channel.send('', {
-              embed: {
-                  "title" : `${bannedPerson.user.id} has been unbanned from the server.`,
-                  "color" : 0x00FF00
-              }
-          })
-        } catch(e) {
-          console.log(e.message);
-        }
-    },
-    description: 'Unbans a user',
-  	detailed: 'Unbans mentioned user',
-  	examples: prefix => `${prefix}unban <user id>`,
-  	name: 'unban',
-  	perms: [`BAN_MEMBERS`]
+		console.log(bannedPerson)
+		if (bannedPerson == undefined) {
+			return message.channel.send('', {
+				embed: {
+					"title": "Please provide a user ID to unban",
+					"color": 0xFF0000
+				}
+			});
+		}
+		let reason = args.slice(1).join(" ");
+		if (!reason) {
+			reason = "No reason provided";
+		}
+		try {
+			message.guild.members.unban(bannedPerson.user.id, { Reason: reason })
+			message.channel.send('', {
+				embed: {
+					title: 'User successfully unbanned',
+					description: `${bannedPerson.user.tag} is no longer banned.`,
+					color: 0x10CC10,
+					footer: {
+						text: `id: ${bannedPerson.user.id}`
+					}
+				}
+			})
+		} catch (e) {
+			console.log(e.message);
+		}
+	},
+	description: 'Unbans a user',
+	detailed: 'Unbans mentioned user',
+	examples: prefix => `${prefix}unban <user id>`,
+	name: 'unban',
+	perms: [`BAN_MEMBERS`]
 }
