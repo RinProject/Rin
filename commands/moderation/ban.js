cfd69fa8886d1f805f8da54a14f05b0cfb978a2b
ItsSoze <61681911+ItsSoze@users.noreply.github.com>
2020-05-25 13:55:33 -0400

    Reverted ban.js
    - Removed all changes made by me in ban.js today, due to failure to create a working catch

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,53 +1,28 @@
 module.exports = {
 	async run(message, args) {
-	// Check if bot has permissions to ban users
-        if(!message.guild.me.hasPermission("BAN_MEMBERS")) {
-            return message.channel.send('', {
-				embed: {
-						"title" : "An error occurred.",
-						"description": "I require the **ban members** permission to ban users.",
-						"color" : 0xFF0000
-				}
-			})
-        }
-			let user = message.mentions.users.first() || await message.client.users.fetch(args[1])
-			.catch(e => {
-				if (e) {
-				    message.channel.send('', {
+			let member = message.mentions.members.first() || await message.guild.members.cache.get(args[1]);
+			if (member == undefined) {
+				return message.channel.send('', {
 					embed: {
-							"title" : "Please provide a user to ban.",
+							"title" : "Mention a user to ban",
 							"color" : 0xFF0000
 					}
-				})}
-			})
-
-			if (user == undefined)
-			{
-				return;
+				});
 			}
-			
-					message.guild.members.ban(user, 0).then(() => {
+			else {
+					member.ban(0).then(() => {
 						message.channel.send('', {
 							embed: {
-									"title" : `${user.tag} has been banned by ${message.author.tag}.`,
+									"title" : `${member.user.tag} has been banned by ${message.author.tag}.`,
 									"color" : 0x00FF00
-							}
-						}).catch(e => {
-							if (e) {
-								return message.channel.send('', {
-									embed: {
-											"title" : "An error occurred.",
-											"description": "The bot is unable to ban the given user, please check it's position in the hierarchy.",
-											"color" : 0xFF0000
-									}
-								})
 							}
 						});
 					})
+			}
 	},
 	description: 'Bans a user',
-	detailed: 'Bans a user by ID or mention.',
+	detailed: 'Bans all users mentioned',
 	examples: prefix => `${prefix}ban @someone, ${prefix}ban <id>`,
 	name: 'ban',
-	perms: ['BAN_MEMBERS']
+	perms: [`BAN_MEMBERS`]
 }
