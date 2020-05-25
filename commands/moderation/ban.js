f3805a43571ffa0a0d6de4c1eb119456ca44c716
ItsSoze <61681911+ItsSoze@users.noreply.github.com>
2020-05-25 11:33:24 -0400

    Updated ban.js
    + Made a proper catch for checking if the user given is bannable (discord.js bad)
    + Changed variable member to user for better understanding of code for readers

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -10,7 +10,7 @@ module.exports = {
 				}
 			})
         }
-			let member = message.mentions.users.first() || await message.client.users.fetch(args[1])
+			let user = message.mentions.users.first() || await message.client.users.fetch(args[1])
 			.catch(e => {
 				if (e) {
 				    message.channel.send('', {
@@ -21,29 +21,29 @@ module.exports = {
 				})}
 			})
 
-			if (member == undefined)
+			if (user == undefined)
 			{
 				return;
 			}
-
-			if(!member.bannable) {
-				return message.channel.send('', {
-				embed: {
-						"title" : "An error occurred.",
-						"description": "The bot is unable to ban the given user, please check it's position in the hierarchy.",
-						"color" : 0xFF0000
-				}
-			})}
-
-			else {
-					message.guild.members.ban(member, 0).then(() => {
+			
+					message.guild.members.ban(user, 0).then(() => {
 						message.channel.send('', {
 							embed: {
-									"title" : `${member.tag} has been banned by ${message.author.tag}.`,
+									"title" : `${user.tag} has been banned by ${message.author.tag}.`,
 									"color" : 0x00FF00
 							}
+						}).catch(e => {
+							if (e) {
+								return message.channel.send('', {
+									embed: {
+											"title" : "An error occurred.",
+											"description": "The bot is unable to ban the given user, please check it's position in the hierarchy.",
+											"color" : 0xFF0000
+									}
+								})
+							}
 						});
-					})}
+					})
 	},
 	description: 'Bans a user',
 	detailed: 'Bans a user by ID or mention.',
