7e6c81f3ddf34a9ddeb4e78d3af91dfd0ddd6079
ItsSoze <61681911+ItsSoze@users.noreply.github.com>
2020-05-25 11:11:13 -0400

    Updated ban.js
    + Bot now supports banning members it has not interacted with (fetches across Discord)
    + Bot checks for it's own permission to ban users
    + Bot checks if a member is bannable before attempting ban
    - Removed check for an undefined member due to new method of fetching
    - No longer gets member from cache for variable member
    - Variable member no longer takes in a GuildMember, but a User

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,28 +1,53 @@
 module.exports = {
 	async run(message, args) {
-			let member = message.mentions.members.first() || await message.guild.members.cache.get(args[1]);
-			if (member == undefined) {
-				return message.channel.send('', {
+	// Check if bot has permissions to ban users
+        if(!message.guild.me.hasPermission("BAN_MEMBERS")) {
+            return message.channel.send('', {
+				embed: {
+						"title" : "An error occurred.",
+						"description": "I require the **ban members** permission to ban users.",
+						"color" : 0xFF0000
+				}
+			})
+        }
+			let member = message.mentions.users.first() || await message.client.users.fetch(args[1])
+			.catch(e => {
+				if (e) {
+				    message.channel.send('', {
 					embed: {
-							"title" : "Mention a user to ban",
+							"title" : "Please provide a user to ban.",
 							"color" : 0xFF0000
 					}
-				});
+				})}
+			})
+
+			if (member == undefined)
+			{
+				return;
 			}
+
+			if(!member.bannable) {
+				return message.channel.send('', {
+				embed: {
+						"title" : "An error occurred.",
+						"description": "The bot is unable to ban the given user, please check it's position in the hierarchy.",
+						"color" : 0xFF0000
+				}
+			})}
+
 			else {
-					member.ban(0).then(() => {
+					message.guild.members.ban(member, 0).then(() => {
 						message.channel.send('', {
 							embed: {
-									"title" : `${member.user.tag} has been banned by ${message.author.tag}.`,
+									"title" : `${member.tag} has been banned by ${message.author.tag}.`,
 									"color" : 0x00FF00
 							}
 						});
-					})
-			}
+					})}
 	},
 	description: 'Bans a user',
-	detailed: 'Bans all users mentioned',
+	detailed: 'Bans a user by ID or mention.',
 	examples: prefix => `${prefix}ban @someone, ${prefix}ban <id>`,
 	name: 'ban',
-	perms: [`BAN_MEMBERS`]
+	perms: ['BAN_MEMBERS']
 }
