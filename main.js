ab8677512ad60d5923c6c939304dfce884a2fecb
Tarren <26067973+EpokTarren@users.noreply.github.com>
2020-05-25 19:34:54 +0200

    Added message edit logging
    Accidentally forgot to implement this

--- main.js
+++ main.js
@@ -89,6 +89,41 @@ client.on('messageDeleteBulk', (messages)=>{
 	});
 });
 
+client.on('messageUpdate', (oldMessage, newMessage)=>{
+	if(!newMessage.guild) return;
+	db.all(`SELECT messageUpdate, messageLogChannel FROM logs WHERE guild = "${newMessage.channel.guild.id}"`, (err, rows)=>{
+		if(rows[0] && rows[0]['messageUpdate'])
+			client.channels.cache.get(rows[0]['messageLogChannel']).send({
+				embed:{
+					author:{
+						name: newMessage.author.tag,
+						iconURL: newMessage.author.avatarURL()
+					},
+					color: 0xcc1020,
+					title: 'Message edited',
+					description: `Channel: ${newMessage.channel.toString()}\n[Message](${newMessage.url})`,
+					fields: [
+						{
+							name: 'Pre edit message',
+							inline: true,
+							value: oldMessage.content
+						},
+						{
+							name: 'Edited message',
+							inline: true,
+							value: newMessage.content
+						}
+					],
+					footer: {
+						text: `uid: ${newMessage.author.id}`
+					}
+				}
+			});
+		if(err)
+			errorLog(err);
+	});
+});
+
 //Message reaction logs
 client.on('messageReactionAdd', (messageReaction, user)=>{
 	if(!messageReaction.message.guild) return;
