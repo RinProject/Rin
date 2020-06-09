e705899b41ca0025259816e87c9bed6127764116
vAnomali <54195092+vAnomali@users.noreply.github.com>
2020-06-09 13:50:08 -0400

    fixed it to It

--- commands/fun/kiss.js
+++ commands/fun/kiss.js
@@ -1,10 +1,10 @@
-const images = require('../../JSONStorage/hug.json')
+const images = require('../../JSONStorage/kiss.json')
 module.exports = {
 	run: async function (message) {
 		if(message.mentions.users.first())
 			message.channel.send('', {
 				embed: {
-					title: `${message.mentions.users.first().username} has been hugged by ${message.author.username}`,
+					title: `${message.mentions.users.first().username} has been kissed by ${message.author.username}`,
 					color: 0xff80cc,
 					image: {
 						url: images[Math.floor(Math.random() * images.length)]
@@ -15,14 +15,14 @@ module.exports = {
 			message.channel.send('', {
 				embed: {
 					title: 'An error has occurred',
-					description: 'It appears the person you have tried to hug does not exist.',
+					description: 'it appears the person you have tried to kiss does not exist.',
 					color: 0xff0000
 				}
 			});
 	},
-	description: 'Hugs a user',
-	detailed: 'Hugs first mentioned user',
-	examples: prefix => `${prefix}hug @someone`,
-	name: 'hug',
+	description: 'Kisses a user',
+	detailed: 'Kisses first mentioned user',
+	examples: prefix => `${prefix}kiss @someone`,
+	name: 'kiss',
 	perms: null,
 };
