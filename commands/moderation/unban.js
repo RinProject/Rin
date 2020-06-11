1e80247fc70a32d01b775e6e1b23cb57dd55620a
ItsSoze <61681911+ItsSoze@users.noreply.github.com>
2020-06-11 03:17:35 -0400

    Fixed & c;leaned unban.js
    - Removed a console log
    - Bot properly takes in a reason now
    - Added periods in sentences where necessary

--- commands/moderation/unban.js
+++ commands/moderation/unban.js
@@ -2,22 +2,21 @@ module.exports = {
 	async run(message, args) {
 		let bans = await message.guild.fetchBans();
 		let bannedPerson = bans.get(args[1]);
-
-		console.log(bannedPerson)
+		
 		if(bannedPerson == undefined) {
 			return message.channel.send('', {
 				embed: {
-					title: 'Please provide a user ID to unban',
+					title: 'Please provide a user ID to unban.',
 					color: 0xFF0000
 				}
 			});
 		}
-		let reason = args.slice(1).join(" ");
+		let reason = args.slice(2).join(" ");
 		if(!reason) {
-			reason = "No reason provided";
+			reason = "No reason provided.";
 		}
 		try {
-			message.guild.members.unban(bannedPerson.user.id, { Reason: reason });
+			message.guild.members.unban(bannedPerson.user.id, reason);
 			message.channel.send('', {
 				embed: {
 					title: 'User successfully unbanned',
