ebe2303465109bd676b8d482a4f13cc69fbca5a8
ItsSoze <61681911+ItsSoze@users.noreply.github.com>
2020-06-23 01:52:48 -0400

    1v1'ed reactroles.js again
    - Moved ID variables around
    - Removed other needless code
    - Am I forgetting something?

--- commands/utility/reactroles.js
+++ commands/utility/reactroles.js
@@ -21,42 +21,33 @@ module.exports = {
                     description: `Please follow the format ${config.prefix}reactrole add <messageid> <emojiid> <roleid> or ${config.prefix}reactrole remove <messageid> <emojiid>.`
                 }
             })
-        } 
+        }  
+        
+        // ID variables (essentially arguments) put in by user, used for later
+        let messageId = args[2];
+        let emojiId = args[3];
+        let roleId = args[4];
+
         if (args[1] == 'add') {
-            if (args[2] == undefined || args[3] == undefined || args[4] == undefined) {
+            if (messageId == undefined || emojiId == undefined || roleId == undefined) {
                 return message.channel.send('', {
                     embed: {
                         color: 0xFF0000,
                         description: `Please follow the format ${config.prefix}reactrole add <messageid> <emojiid> <roleid>.`
                     }
                 });
-            } else {
-                // IDs put in by user, used for later
-                let messageId = args[2];
-                let emojiId = args[3];
-                let roleId = args[4];
+            } 
 
-                // Fetches message, emoji, and role by given IDs.
-                let fetchedMessage = message.channel.messages.fetch(args[2]).then(m => {fetchedMessage = m});
-                let fetchedEmoji = message.guild.emojis.cache.get(emojiId);
-                let fetchedRole = message.guild.roles.cache.get(roleId);
-            }
-            if (fetchedMessage == undefined || fetchedEmoji == undefined || fetchedRole == undefined) return;
+            // Reacts to given message
+            message.channel.messages.fetch(messageId).then(m => {m.react(emojiId)});
             
-            if (fetchedMessage != message.guild.id) {
-                return message.channel.send('', {
-                    embed: {
-                        color: 0xFF0000,
-                        description: "You can only add a react role to messages in this server."
-                    }
-                });
-            } else { 
+            // Does database things
                 db.run(`INSERT OR REPLACE INTO reactroles(messageid, emojiid, roleid) VALUES (${messageId}, ${emojiId}, ${roleId})`);
                 db.all(`SELECT * FROM reactroles WHERE messageid = ${messageId} AND emojiid = ${emojiId} AND roleid = ${roleId}`, (err, rows) => {
                     if(err)
                         console.log(err);  
                 });
-            }
+            
         } 
         if (args[1] == 'remove') {
             if (args[2] == undefined || args[3] == undefined){
