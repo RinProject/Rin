1bd6d3508dbf20c91dc8363f9d0d5d5d81e516e7
ItsSoze <61681911+ItsSoze@users.noreply.github.com>
2020-07-20 20:27:47 -0400

    Update hug.js
    - ID support implemented
    - New response if user tries to hug themselves
    - Updated example

--- commands/fun/hug.js
+++ commands/fun/hug.js
@@ -1,18 +1,36 @@
 const images = require('../../JSONStorage/hug.json')
 module.exports = {
-	run: async function (message) {
-		if(message.mentions.users.first())
-			message.channel.send('', {
+	run: async function (message, args) {
+		let member = message.mentions.members.first() || await message.guild.members.cache.get(args[1]);
+		
+		if(member) {
+		if (message.author.id == member.user.id) {
+			return message.channel.send('', {
 				embed: {
-					title: `${message.mentions.users.first().username} has been hugged by ${message.author.username}`,
+					title: `${member.user.tag} has been hugged by ${message.client.user.tag}`,
+					description: `You tried to hug yourself? Don't worry, it'll be okay.`,
 					color: 0xff80cc,
 					image: {
 						url: images[Math.floor(Math.random() * images.length)]
 					}
 				}
-			});
+			})
+		}
+
+		else {
+			return message.channel.send('', {
+				embed: {
+					title: `${member.user.tag} has been hugged by ${message.author.tag}`,
+					color: 0xff80cc,
+					image: {
+						url: images[Math.floor(Math.random() * images.length)]
+					}
+				}
+			})}
+
+		}
 		else
-			message.channel.send('', {
+			return message.channel.send('', {
 				embed: {
 					title: 'An error has occurred',
 					description: 'It appears the person you have tried to hug does not exist.',
@@ -22,7 +40,7 @@ module.exports = {
 	},
 	description: 'Hugs a user',
 	detailed: 'Hugs first mentioned user',
-	examples: prefix => `${prefix}hug @someone`,
+	examples: prefix => `${prefix}hug @member`,
 	name: 'hug',
 	perms: null,
 	guildOnly: true
