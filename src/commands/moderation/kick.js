c694b7b47bb794e861ef09172f4b34bae0efed08
ItsSoze <61681911+ItsSoze@users.noreply.github.com>
2020-08-26 17:44:14 -0400

    kick.js now uses reason functionality
    - Added optional reason input for kicking a member
    - Changed "user" to "member" where found, to better represent the type being used for variable member
    - Updated description "detailed" and examples to reflect reason funtionality

--- src/commands/moderation/kick.js
+++ src/commands/moderation/kick.js
@@ -1,37 +1,46 @@
 module.exports = {
 	async run(message, args) {
 		let member = message.mentions.members.first() || await message.guild.members.cache.get(args[1]);
+		let reason = args.slice(2).join(" ");
+		if (!reason) reason = `No reason provided. Responsible moderator: ${message.author.tag}`;
+
 		if(member == undefined) {
 			return message.channel.send('', {
 				embed: {
-					title: 'Mention a user to kick',
+					title: 'An error occurred',
+					description: 'No member was provided to kick.',
 					color: colors.error
 				}
 			});
 		}
+
 		if(!member.kickable) {
 			return message.channel.send('', {
 				embed: {
 					title: 'An error occurred.',
-					description: 'The bot is unable to kick the given user, please check it\'s position in the hierarchy.',
+					description: 'The bot is unable to kick the given member, please check it\'s position in the hierarchy.',
 					color: colors.error
 				}
 			});
 		}
 		else {
-			member.kick().then(() => {
+			member.kick(reason).then(() => {
 				message.channel.send('', {
 					embed: {
-						title: `${member.user.tag} has been kicked by ${message.author.tag}.`,
-						color: colors.negative
+						title: 'Member successfully kicked',
+						description: `${member.user.tag} has been kicked by ${message.author.tag}.`,
+						color: colors.negative,
+						footer: {
+							text: `id: ${member.id}`
+						}
 					}
 				});
 			});
 		}
 	},
-	description: 'Kicks a user',
-	detailed: 'Kicks mentioned user',
-	examples: prefix => `${prefix}kick @someone, ${prefix}kick <id>`,
+	description: 'Kicks a givem member',
+	detailed: 'Kicks given member with the option to add a reason for the kick.',
+	examples: prefix => `${prefix}kick @someone reason, ${prefix}kick <id> reason`,
 	name: 'kick',
 	perms: ['KICK_MEMBERS'],
 	botPerms: ['KICK_MEMBERS'],
