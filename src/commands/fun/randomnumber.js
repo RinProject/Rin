8eaa04f398764744c0b03b25d1656d0ed27cb3e1
vAnomali <54195092+vAnomali@users.noreply.github.com>
2020-09-08 11:50:57 -0400

    Create randomnumber.js

---
+++ src/commands/fun/randomnumber.js
@@ -0,0 +1,25 @@
+let allowedMax = Math.pow(2, 33); //Max number allowed it 2^33
+let randNum = 0; 
+
+module.exports = {
+	async run(message, args) {
+        let n = parseInt(args[1]) || 100;
+        if (n > allowedMax) 
+            n = allowedMax;
+        randNum = Math.floor(Math.random() * n) + 1;
+        return message.channel.send('', {
+            embed: { 
+                title: 'Random number generated',
+                description: `${randNum}`,
+                color: colors.positive
+            }
+        });
+	},
+	aliases: ['randnum'],
+	description: 'Returns a random number.',
+	detailed: 'Returns a random number in a given range or in a bot generated maximum.',
+	examples: prefix => `${prefix}randomnumber, ${prefix}randomnumber 1000`,
+	name: 'randomnumber',
+	perms: null,
+    guildOnly: true
+}
