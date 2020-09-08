75a793a6e1e081617fdecef29dad2cf19f3ac7e4
vAnomali <54195092+vAnomali@users.noreply.github.com>
2020-09-08 04:11:44 -0400

    Added error embed for if no banner exists

--- src/commands/utility/serverbanner.js
+++ src/commands/utility/serverbanner.js
@@ -2,21 +2,30 @@ const options = require('../../JSONStorage/timeFormatOptions.json');
 const formatter = new Intl.DateTimeFormat('en-GB', options);
 module.exports = {
 	async run(message, args) {
-		message.channel.send('', {
-            embed: {
-                title: `Server icon for ${message.guild.name}`,description: `[Server Banner URL](${message.guild.bannerURL({format: "png", size: 512, dynamic: true})})`,
-				color: colors.base,
-				image: {
-					url: message.guild.bannerURL({
-						format: "png",
-						size: 512,
-						dynamic: true
-					}),
-					height: 512, 
-					width: 512
+		if (message.guild.bannerUR)
+			return message.channel.send('', {
+				embed: {
+					title: `Server banner for ${message.guild.name}`,description: `[Server Banner URL](${message.guild.bannerURL({format: "png", size: 512, dynamic: true})})`,
+					color: colors.base,
+					image: {
+						url: message.guild.bannerURL({
+							format: "png",
+							size: 512,
+							dynamic: true
+						}),
+						height: 512, 
+						width: 512
+					}
 				}
-			}
-        });
+			});
+		else 
+			return message.channel.send('', {
+				embed: {
+					title: 'An error has occurred.',
+					description: 'It appears this server does not have a banner.',
+					color: colors.error
+				}
+			});
 	},
 	aliases: ['banner'],
 	description: 'Returns a server banner',
