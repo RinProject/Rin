dc4675776b998990a5a2859bc2210f6d391a58ea
Capuccino <chinodesuuu@gmail.com>
2020-09-20 09:05:44 +0000

    Add event loader in events dir
    Signed-off-by: Capuccino <chinodesuuu@gmail.com>

--- src/events/unmute.js
+++ src/events/unmute.js
@@ -1,3 +1,19 @@
+const { asyncDB } = require("../handler/utils")
+
 module.exports = client => {
-    
+    client.on("unmute", async (g, m) => {
+
+        let logChan = await asyncDB.get(db,
+            "SELECT modLogChannel FROM logs WHERE guild = (?)", 
+            [guild.id]);
+        
+        if (logChan && logChan.modLogChannel)
+           g.channels.resolve(logChan.modLogChannel).send({
+               embed: {
+                   title: "User unmuted",
+                   description: `${member.toString()} unmuted`,
+                   color: 0x80FF80
+               }
+           })
+    })
 }
